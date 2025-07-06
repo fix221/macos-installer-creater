105e9f7eaf87a09e1c5fc43ce83983938e694365
Kevin Wang <wangpenghao69@gmail.com>
2025-07-06 22:38:57 +0800

    modified extract_urls to get

--- extract_urls.py
+++ extract_urls.py
@@ -3,18 +3,10 @@ import requests
 import os
 
 def parse_catalog(file_path):
-    """ 解析XML目录文件并提取pkg URL。 """
-    urls = []
     tree = ET.parse(file_path)
     root = tree.getroot()
-    products = root.find('dict/dict/key[text()="Products"]/following-sibling::dict')
-
-    for product in products:
-        for package in product.find('dict/key[text()="Packages"]/following-sibling::array'):
-            url = package.find('dict/key[text()="URL"]/following-sibling::string').text
-            if url.endswith('.pkg'):
-                urls.append(url)
-
+    # 假设你需要从 XML 中提取 URL，这里只是一个示例
+    urls = [elem.text for elem in root.findall('.//url')]
     return urls
 
 def save_urls_to_file(urls, output_file='pkg_urls.txt'):
@@ -29,8 +21,14 @@ def main():
     #https://swscan.apple.com/content/catalogs/others/index-26seed-26-15-14-13-12-10.16-10.15-10.14-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog
     catalog_file = 'https://swscan.apple.com/content/catalogs/others/index-26seed-26-15-14-13-12-10.16-10.15-10.14-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog'
     file = requests.get(catalog_file)
+    file_path = os.path.join(os.getcwd(), 'index-26seed-26-15-14-13-12-10.16-10.15-10.14-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.xml')
+    with open(file_path, 'wb') as f:
+    # with open('index-26seed-26-15-14-13-12-10.16-10.15-10.14-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.xml', 'wb') as f:
+        f.write(file.content)
+
+    # local file
     # catalog_file = os.path.join(os.getcwd(), 'index-26seed-26-15-14-13-12-10.16-10.15-10.14-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog')
-    urls = parse_catalog(catalog_file)
+    urls = parse_catalog(file_path)
     save_urls_to_file(urls)
 
 if __name__ == "__main__":
